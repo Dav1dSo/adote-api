33d3a17c4dbce602750c112fb7e516caa2e9f65e
David Soares <davidsoares.dev@gmail.com>
2024-10-05 23:33:07 -0300

    Fix: Ensure connection established in test
    The `get_engine` method was incorrectly asserting the engine to be `None`, preventing the test from working correctly. This commit corrects the assertion to ensure the engine is not `None`, confirming the connection has been established.

--- src/models/mysql/settings/connection_test.py
+++ src/models/mysql/settings/connection_test.py
@@ -2,7 +2,7 @@ from .connection import db_connection_handler
 from sqlalchemy.engine import Engine
 
 def test_connect_to_db():
-    assert db_connection_handler.get_engine() is None
+    assert db_connection_handler.get_engine() is not None
     
     db_connection_handler.connect_to_db()
     db_engine = db_connection_handler.get_engine()
